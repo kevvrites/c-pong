f3a235600b45e15e7f8a72939a2db36c50821e9b
Kevin Liu <liukproductivity@gmail.com>
2024-04-19 14:48:51 -0400

    added remap controls, refactored game, updated makefiles

--- pong.c
+++ pong.c
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include <stdlib.h>
+#include <math.h>
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 400
@@ -7,6 +9,7 @@
 #define BALL_SIZE 10
 #define PADDLE_SPEED 5
 #define BALL_SPEED 3
+#define MAX_BALL_SPEED 7
 #define WIN_CON 5
 
 typedef struct {
@@ -21,14 +24,14 @@ typedef struct {
 
 typedef enum {
     START_SCREEN,
-    HELP_SCREEN,
+    CONTROLS_SCREEN,
     GAME_SCREEN,
     END_SCREEN
 } GameState;
 
 void startScreen(GameState *state);
-void helpScreen(GameState *state);
-void gameScreen(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, bool *gameOver, int *winner);
+void controlsScreen(GameState *state, int *p1Up, int *p1Down, int *p2Up, int *p2Down);
+void gameScreen(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, bool *gameOver, int *winner, int p1Up, int p1Down, int p2Up, int p2Down);
 void endScreen(GameState *state, int winner, int leftScore, int rightScore, bool *gameOver, Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball);
 
 void startScreen(GameState *state) {
@@ -46,30 +49,78 @@ void startScreen(GameState *state) {
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         Vector2 mousePos = GetMousePosition();
         if (mousePos.x >= WINDOW_WIDTH - 30 && mousePos.x <= WINDOW_WIDTH - 10 && mousePos.y >= 20 && mousePos.y <= 40) {
-            *state = HELP_SCREEN;
+            *state = CONTROLS_SCREEN;
         }
     }
 }
 
-void helpScreen(GameState *state) {
-    DrawText("Controls", WINDOW_WIDTH / 2 - MeasureText("Controls", 30) / 2, WINDOW_HEIGHT / 4, 30, WHITE);
-    DrawText("Left Paddle: W/S", WINDOW_WIDTH / 2 - MeasureText("Left Paddle: W/S", 20) / 2, WINDOW_HEIGHT / 2 - 20, 20, WHITE);
-    DrawText("Right Paddle: Up/Down", WINDOW_WIDTH / 2 - MeasureText("Right Paddle: Up/Down", 20) / 2, WINDOW_HEIGHT / 2 + 20, 20, WHITE);
+void controlsScreen(GameState *state, int *p1Up, int *p1Down, int *p2Up, int *p2Down) {
+    DrawText("Remap Controls", WINDOW_WIDTH / 2 - MeasureText("Remap Controls", 30) / 2, WINDOW_HEIGHT / 4, 30, WHITE);
+    DrawText("Press the key for each control", WINDOW_WIDTH / 2 - MeasureText("Press the key for each control", 20) / 2, WINDOW_HEIGHT / 2 - 60, 20, WHITE);
+    
+    DrawText("P1 Up:", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 - 20, 20, WHITE);
+    DrawText("P1 Down:", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 + 20, 20, WHITE);
+    DrawText("P2 Up:", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 + 60, 20, WHITE);
+    DrawText("P2 Down:", WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2 + 100, 20, WHITE);
+    
+    static int selectedControl = -1;
+    
+    for (int i = 0; i < 4; i++) {
+        int key = (i == 0) ? *p1Up : (i == 1) ? *p1Down : (i == 2) ? *p2Up : *p2Down;
+        const char* keyText = (key == KEY_UP) ? "UP" : (key == KEY_DOWN) ? "DOWN" : TextFormat("%c", key);
+        
+        if (i == selectedControl) {
+            DrawText(keyText, WINDOW_WIDTH / 2 + 50, WINDOW_HEIGHT / 2 - 20 + i * 40, 20, WHITE);
+        } else {
+            DrawRectangleLines(WINDOW_WIDTH / 2 + 45, WINDOW_HEIGHT / 2 - 25 + i * 40, 80, 30, WHITE);
+            DrawText(keyText, WINDOW_WIDTH / 2 + 50, WINDOW_HEIGHT / 2 - 20 + i * 40, 20, WHITE);
+        }
+    }
+    
     DrawText("Press SPACE to return", WINDOW_WIDTH / 2 - MeasureText("Press SPACE to return", 20) / 2, WINDOW_HEIGHT - 40, 20, WHITE);
-
+    
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        if (GetMouseX() >= WINDOW_WIDTH / 2 + 45 && GetMouseX() <= WINDOW_WIDTH / 2 + 75) {
+            if (GetMouseY() >= WINDOW_HEIGHT / 2 - 25 && GetMouseY() <= WINDOW_HEIGHT / 2 + 5)
+                selectedControl = 0;
+            else if (GetMouseY() >= WINDOW_HEIGHT / 2 + 15 && GetMouseY() <= WINDOW_HEIGHT / 2 + 45)
+                selectedControl = 1;
+            else if (GetMouseY() >= WINDOW_HEIGHT / 2 + 55 && GetMouseY() <= WINDOW_HEIGHT / 2 + 85)
+                selectedControl = 2;
+            else if (GetMouseY() >= WINDOW_HEIGHT / 2 + 95 && GetMouseY() <= WINDOW_HEIGHT / 2 + 125)
+                selectedControl = 3;
+        }
+    }
+    
+    if (selectedControl >= 0) {
+        int key = GetKeyPressed();
+        if (key > 0) {
+            if (selectedControl == 0)
+                *p1Up = key;
+            else if (selectedControl == 1)
+                *p1Down = key;
+            else if (selectedControl == 2)
+                *p2Up = key;
+            else if (selectedControl == 3)
+                *p2Down = key;
+            
+            selectedControl = -1;
+        }
+    }
+    
     if (IsKeyPressed(KEY_SPACE)) {
         *state = START_SCREEN;
     }
 }
 
-void gameScreen(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, bool *gameOver, int *winner) {
-    if (IsKeyDown(KEY_W) && leftPaddle->position.y > 0)
+void gameScreen(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, bool *gameOver, int *winner, int p1Up, int p1Down, int p2Up, int p2Down) {
+    if (IsKeyDown(p1Up) && leftPaddle->position.y > 0)
         leftPaddle->position.y -= PADDLE_SPEED;
-    if (IsKeyDown(KEY_S) && leftPaddle->position.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
+    if (IsKeyDown(p1Down) && leftPaddle->position.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
         leftPaddle->position.y += PADDLE_SPEED;
-    if (IsKeyDown(KEY_UP) && rightPaddle->position.y > 0)
+    if (IsKeyDown(p2Up) && rightPaddle->position.y > 0)
         rightPaddle->position.y -= PADDLE_SPEED;
-    if (IsKeyDown(KEY_DOWN) && rightPaddle->position.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
+    if (IsKeyDown(p2Down) && rightPaddle->position.y < WINDOW_HEIGHT - PADDLE_HEIGHT)
         rightPaddle->position.y += PADDLE_SPEED;
 
     ball->position.x += ball->speed.x;
@@ -79,11 +130,19 @@ void gameScreen(Paddle *leftPaddle, Paddle *rightPaddle, Ball *ball, bool *gameO
         ball->speed.y = -ball->speed.y;
 
     if (CheckCollisionRecs((Rectangle){ leftPaddle->position.x, leftPaddle->position.y, PADDLE_WIDTH, PADDLE_HEIGHT },
-                            (Rectangle){ ball->position.x, ball->position.y, BALL_SIZE, BALL_SIZE }))
+                            (Rectangle){ ball->position.x, ball->position.y, BALL_SIZE, BALL_SIZE })) {
         ball->speed.x = -ball->speed.x;
+        ball->position.x = leftPaddle->position.x + PADDLE_WIDTH;
+        if (fabsf(ball->speed.x) < MAX_BALL_SPEED)
+            ball->speed.x *= 1.1f;
+    }
     if (CheckCollisionRecs((Rectangle){ rightPaddle->position.x, rightPaddle->position.y, PADDLE_WIDTH, PADDLE_HEIGHT },
-                            (Rectangle){ ball->position.x, ball->position.y, BALL_SIZE, BALL_SIZE }))
+                            (Rectangle){ ball->position.x, ball->position.y, BALL_SIZE, BALL_SIZE })) {
         ball->speed.x = -ball->speed.x;
+        ball->position.x = rightPaddle->position.x - BALL_SIZE;
+        if (fabsf(ball->speed.x) < MAX_BALL_SPEED)
+            ball->speed.x *= 1.1f;
+    }
 
     if (ball->position.x < 0) {
         rightPaddle->score++;
@@ -154,6 +213,11 @@ int main() {
 
     bool gameOver = false;
     int winner = 0;
+    
+    int p1Up = KEY_W;
+    int p1Down = KEY_S;
+    int p2Up = KEY_UP;
+    int p2Down = KEY_DOWN;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -163,11 +227,11 @@ int main() {
             case START_SCREEN:
                 startScreen(&state);
                 break;
-            case HELP_SCREEN:
-                helpScreen(&state);
+            case CONTROLS_SCREEN:
+                controlsScreen(&state, &p1Up, &p1Down, &p2Up, &p2Down);
                 break;
             case GAME_SCREEN:
-                gameScreen(&leftPaddle, &rightPaddle, &ball, &gameOver, &winner);
+                gameScreen(&leftPaddle, &rightPaddle, &ball, &gameOver, &winner, p1Up, p1Down, p2Up, p2Down);
                 if (gameOver) {
                     state = END_SCREEN;
                 }
--- todo.md
+++ todo.md
@@ -1,20 +1,20 @@
 Pong
 
 - ~~Start Screen~~
-  - Animation of game in background
+  - ~~Animation of game in background~~ pong basic
   - ~~Press space to start~~
   - ~~Help/Controls~~
 - Help/Controls Screen
   - ~~Controls~~
-  - Remap controls
-  - Change win condition (Click/hold +/-1 triangle arrow buttons)
-  - Ball goes to which side when point scored? (winners ball/losers ball)
+  - ~~Remap controls~~
+  - ~~Change win condition (Click/hold +/-1 triangle arrow buttons)~~ first to 5 is fair and not too long
+  - ~~Ball goes to which side when point scored? (winners ball/losers ball)~~ more fair to losers
 - Game
 
   - ~~Left paddle~~
   - ~~Right paddle~~
   - ~~Ball~~
-  - Bounce physics: speed up after successful hits?
-  - Delay ball movement after scoring
+  - ~~Bounce physics: speed up after successful hits~~
+  - ~~Delay ball movement after scoring~~ skill issue, don't want paddles to freeze
 
-- Web Version - in development!
+- ~~Web Version - in development!~~
